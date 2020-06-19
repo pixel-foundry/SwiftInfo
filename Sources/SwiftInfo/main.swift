4c27ec3392e9ff0822c5cf6b572c0be1785912e8
Hal Lee <hal@lee.me>
2020-06-19 15:28:01 -0400

    Update main.swift

--- Sources/SwiftInfo/main.swift
+++ Sources/SwiftInfo/main.swift
@@ -15,7 +15,7 @@ public struct Main {
         log("Infofile Path: \(try! fileUtils.infofileFolder())", verbose: true)
         log("Toolchain Path: \(toolchainPath)", verbose: true)
 
-        throw SwiftInfoError.generic("Tool folder: \(fileUtils.toolFolder)")
+        fatalError("Tool folder: \(fileUtils.toolFolder)")
 
         let processInfoArgs = ProcessInfo.processInfo.arguments
         let args = Runner.getCoreSwiftCArguments(fileUtils: fileUtils,
