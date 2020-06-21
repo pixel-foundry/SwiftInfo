2994f2b375b0b703a68b260351b301c18bd3d165
Hal Lee <hal@lee.me>
2020-06-21 11:16:02 -0400

    Update FileOpener.swift

--- Sources/SwiftInfoCore/FileUtils/FileOpener.swift
+++ Sources/SwiftInfoCore/FileUtils/FileOpener.swift
@@ -11,7 +11,7 @@ open class FileOpener {
 
     open func plistContents(ofPath path: String) -> [String: Any]? {
         SwiftInfoCore.log(path)
-        guard let plistData = FileManager.default.contents(atPath: path) else { fatalError("Couldn't load Plist from \(path)") }
+        guard let plistData = FileManager.default.contents(atPath: path) else { return nil }
         guard let plist = try? PropertyListSerialization.propertyList(
             from: plistData,
             options: [],
